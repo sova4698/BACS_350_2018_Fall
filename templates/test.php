24734709d95df603c44cf850304686cc84ef3b20
Mark Seaman <Mark.Seaman@imac.net>
2018-09-14 10:41:38 -0600

    350 11

--- templates/test.php
+++ templates/test.php
@@ -2,7 +2,8 @@
 
     // Decide on the domain and page to test
     $domain = "https://unco-bacs.org";
-    $page = "$domain/bacs_350/demo/index.php";
+    $page = "bacs_350/demo/index.php";
+    $url = "$domain/$page";
 
     // Create a link for the page
     echo "<h2>Test Page for $page</h2>
@@ -10,9 +11,14 @@
         <p>Make sure that the page exists and has valid HTML.";
 
     echo '<h3>Page Exists</h3>
-        <p><a href="' . $page . '">' . $page . '</a></p>';
+        <p><a href="' . $url . '">' . $page . '</a></p>';
 
     // Run your page through the validator
-    echo '<h3>Valid HTML</h3>
-        <p><a href="http://validator.w3.org">HTML Validator</a></p>'
+    $validator = 'http://validator.w3.org';
+    echo "<h3>Valid HTML</h3><p><a href=\"$validator\">HTML Validator</a></p>";
+
+    $validator_url = "$validator/nu/?doc=$url";
+    echo "<p><a href=\"$validator_url\">Validate Page: $url</a></p>";
+
+
 ?>
