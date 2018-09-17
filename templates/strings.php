023e50343bdb91a0298ee9c67a6b453c647693fd
Mark Seaman <mark.seaman@shrinking-world.com>
2018-09-17 15:21:37 -0600

    Create new table

--- templates/strings.php
+++ templates/strings.php
@@ -18,4 +18,9 @@
     echo date('l, F, d, Y');
     echo date('Y-m-d g:i a');
 
+    <a href=\"$url\">Visit $url</a>
+
+    $validator_url = "$validator/nu/?doc=$url";
+    echo "<a href=\"$validator_url\">Validate Page: $url</a>"
+    
 ?>
