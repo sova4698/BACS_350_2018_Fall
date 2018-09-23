a2608f629099e677aea58fe78daf268e9e923091
Mark Seaman <Mark.Seaman@imac.net>
2018-09-23 09:12:54 -0600

    350 14 - MVC pattern

---
+++ solution/14/index.php
@@ -0,0 +1,35 @@
+ <?php
+
+    // Start the page
+    $page_title = 'BACS 350 - Project #14 - MVC Pattern';
+    require_once '../../header.php';
+
+    echo '
+        <h1>Model - View - Controller</h1>
+
+        <p>
+            The MVC Pattern is a design pattern that makes your code easier to read by adding consistency.
+            This solution is built in a series of steps so that you can see the progression toward the
+            final answer.   Repeat these same steps in building your code.
+        </p>
+        <p>Steps to MVC</p>
+        <ul>
+            <li><a href="step-1">Step 1 - Page Style and Structure</a></li>
+            <li><a href="step-2">Step 2 - Database Connection</a></li>
+            <li><a href="step-3">Step 3 - CRUD</a></li>
+            <li><a href="step-4">Step 4 - Views</a></li>
+            <li><a href="step-5">Step 5 - Controller</a></li>
+            <li><a href="step-6">Step 5 - Testing</a></li>
+        </ul>
+
+    ';
+
+
+    // Show links for page testing
+//    require 'test.php';
+
+
+    // End the page
+    require_once '../../footer.php';
+
+?>
