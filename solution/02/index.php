8b70ad734b17584c2db2fc017ebe1287125b9e07
Mark Seaman <mark.seaman@shrinking-world.com>
2018-09-28 17:37:09 -0600

    Update demo code

--- solution/02/index.php
+++ solution/02/index.php
@@ -1,15 +1,42 @@
 <?php 
     $page_title = 'BACS 350 - Project #2 - Includes';
     include "header.php"; 
-?>
+ ?>
 
 <!--  Your code goes here -->
 
-<img src="Bear3.png">
+
 
 <p>This is a simple HTML page that demonstrates using an Include to capture boiler-plate code.</p>
 <p>This reduces duplication in code.</p>
 <p>Learn about the <a href="pattern.php">Include Design Pattern</a></p>
 
+<h2>Problem</h2>
+
+<p>
+    Many constructs are repeated on different pages of a website. We need an easy way to use code that we have written
+    for several different pages. HTML provides no mechanism that will allow us to reuse HTML code.
+</p>
+<p>
+    One place where this would be handy is in defining a standard HTML structure for every web page in our
+    site. All of the code required to start a page and the code to end a page could be packaged in a way
+    that we would not have to replicate this code.
+</p>
+
+<h2>Solution</h2>
+
+<h3>Header/Footer Alternative</h3>
+<p>
+    Put all of the HTML code that we want to use into two files. <b>header.php</b> holds the start of the page while <b>footer.php</b> holds the end of the page.
+</p>
+
+<p>
+    Define a page with the following PHP code.
+</p>
+<pre>
+        include 'header.php';
+        < !-- Add HTML code here -- >
+        include 'footer.php';
+</pre>
 
 <?php include "footer.php"; ?>
--- solution/03/index.php
+++ solution/03/index.php
@@ -1,25 +1,22 @@
 <?php
 
     // Start the page
-    require_once '/views.php';
+    require_once 'views.php';
 
     $page_title = 'Page Design Pattern Demo';
     begin_page($page_title);
-?>
 
 
-    <!--  Your code goes here -->
-    <a href="../..">Home</a>
-    <h1>BACS 350 - Project #3 - Standard Page Template</h1>
+    // Your page content goes here
 
-    
+    echo '<a href="..">Solutions</a>
+    <h1>BACS 350 - Project #3 - Standard Page Template</h1>';
 
-    <p>This is a simple HTML page that demonstrates using an Include to capture boiler-plate code.</p>
-    <p>This reduces duplication in code.</p>
-    <p>In this demo the <b>$page_title</b> is set as a variable that is used by the <b>header.php</b> file.</p>
-    <p>This page also includes a stylesheet.</p>
+    // Include other content
+    require 'pattern.html';
 
-<?php
-   // End the page
+
+    // End the page
     end_page();
+
 ?>
