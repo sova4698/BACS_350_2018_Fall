336ae4a31e1e9de310913bb721b6839e3cc4facf
Mark Seaman <Mark.Seaman@imac.net>
2018-09-29 08:56:57 -0600

    Design patterns

--- solution/01/index.php
+++ solution/01/index.php
@@ -6,6 +6,9 @@
         
         The Include Pattern inserts content from one HTML or PHP file directly into another PHP
         file being processed.
+
+        A PHP server is required to properly display this content.  If the PHP preprocessor is
+        not running then the source code for this page will be displayed.
     */
 
     include 'pattern.html';
--- solution/02/index.php
+++ solution/02/index.php
@@ -9,7 +9,7 @@
 
 <p>This is a simple HTML page that demonstrates using an Include to capture boiler-plate code.</p>
 <p>This reduces duplication in code.</p>
-<p>Learn about the <a href="pattern.php">Include Design Pattern</a></p>
+<p>Learn about the <a href="https://seamansguide.com/guide/PhpApps/docs/Includes.md">Include Design Pattern</a></p>
 
 <h2>Problem</h2>
 
@@ -25,7 +25,6 @@
 
 <h2>Solution</h2>
 
-<h3>Header/Footer Alternative</h3>
 <p>
     Put all of the HTML code that we want to use into two files. <b>header.php</b> holds the start of the page while <b>footer.php</b> holds the end of the page.
 </p>
