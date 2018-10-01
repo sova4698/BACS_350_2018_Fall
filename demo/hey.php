d5d7947e12386a2760b1d1079db2bec1afbfd21a
Mark Seaman <mark.seaman@shrinking-world.com>
2018-10-01 14:55:11 -0600

    Class demo

---
+++ demo/hey.php
@@ -0,0 +1,8 @@
+ <?php
+    
+    function hey($title, $text) {
+        
+        echo "<h1>$title</h1><p>$text</p>";
+    }
+    
+?>
--- demo/index.php
+++ demo/index.php
@@ -1,22 +1,12 @@
- <?php
+ 
 
-    // Start the page
-    $page_title = 'Subscriber Demo';
-    require_once 'header.php';
+<?php
+    
+    require 'hey.php';
 
-    // Show the list after the insert
-    require 'select.php';
-
-    // Add a record
-    echo '<form action="insert.php" method="get">
-        
-        <p><label>Name:</label> &nbsp; <input type="text" name="name"></p>
-        <p><label>Email:</label> &nbsp; <input type="text" name="email"></p>
-        <p><input type="submit" value="Add New Contact"/></p>
-        
-    </form>';
-
-    // End the page
-    require_once 'footer.php';
-
+    hey('New title', 
+        'Listen to a story
+        bout a man named Jed.
+    ');
+    
 ?>
