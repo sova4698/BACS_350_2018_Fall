8b84adf7b0806bc0113356512460c14831287a96
Mark Seaman <Mark.Seaman@imac.net>
2018-10-05 09:50:30 -0600

    Create object oriented logging solution

--- solution/11/index.php
+++ solution/11/index.php
@@ -17,32 +17,6 @@
     $log->show_log();
 
 
-//    // Render a list of subscribers
-//    $subscribers->show_subscribers();
-//    
-//
-//    // Show the add form
-//    $subscribers->add_form();
-//
-//
-//    // Button to clear
-//    echo '<a href="delete.php">Reset Subscribers</a>';
-//
-//
-//
-//
-//
-//    // Log the page loading
-//    $page = 'solution/11/index.php';
-//    $text = "Loading Page: $page";
-//    require('log.php');
-//
-//
-//    // Include the main page content
-//    echo '<h1>Page Logging Demo</h1>';
-//    require ('log-history');
-//
-
     end_page();
 
  ?>
